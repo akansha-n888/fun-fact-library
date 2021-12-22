4d0c8a563480ee57a79ec662170b1e1438074fe7
akansha-n888 <akanshanagar8@gmail.com>
2021-12-21 22:06:29 -0500

    Ready for heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn fun_fact_lib.wsgi --log-file -
---
+++ fun_facts/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class FunFactsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'fun_facts'
